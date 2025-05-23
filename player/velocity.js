92b3ec6816481a6890dd1d41ae238a96ed6f3b9a
siriusmart <siriusmart@tuta.io>
2025-05-23 16:06:06 +0100

    supports 1.21.5

--- player/velocity.js
+++ player/velocity.js
@@ -1,14 +1,30 @@
-yarnutils.playerVelocity = () => {
-    let Vec3d = Packages.yarnwrap.util.math.Vec3d;
+if (typeof yarn.playerEntity().lastX == "function")
+    yarnutils.playerVelocity = () => {
+        let Vec3d = Packages.yarnwrap.util.math.Vec3d;
 
-    let player = yarn.playerEntity();
-    let tX = player.x - player.prevX();
-    let tY = player.y - player.prevY();
-    let tZ = player.z - player.prevZ();
+        let player = yarn.playerEntity();
+        let tX = player.x - player.lastX();
+        let tY = player.y - player.lastY();
+        let tZ = player.z - player.lastZ();
 
-    tX *= 20;
-    tY *= 20;
-    tZ *= 20;
+        tX *= 20;
+        tY *= 20;
+        tZ *= 20;
 
-    return new Vec3d(tX, tY, tZ);
-};
+        return new Vec3d(tX, tY, tZ);
+    };
+else
+    yarnutils.playerVelocity = () => {
+        let Vec3d = Packages.yarnwrap.util.math.Vec3d;
+
+        let player = yarn.playerEntity();
+        let tX = player.x - player.prevX();
+        let tY = player.y - player.prevY();
+        let tZ = player.z - player.prevZ();
+
+        tX *= 20;
+        tY *= 20;
+        tZ *= 20;
+
+        return new Vec3d(tX, tY, tZ);
+    };
